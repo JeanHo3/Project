a878cc3a3a88d0e1ab08abe62d080c33ba6eb9f1
JeanHo3 <jean.hourmant@gmail.com>
2017-08-27 17:44:02 +0200

    Ajout Iconics

---
+++ IconicsApp/IconicsAppv2/IconicsApp/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+//import java.util.Map;
+//import java.util.TreeMap;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+//		List<Tag> tags = new ArrayList<Tag>();
+		//Map<String, Object[]> data = new TreeMap<String, Object[]>();
+
+		
+		//File output = new File(cheminDestination + "Synthese.txt");
+
+		//	BufferedWriter bw = new BufferedWriter(new FileWriter (output));
+
+		@SuppressWarnings("unused")
+		Fenetre fenetre = new Fenetre();
+
+	//	bw.close();
+	//	GenerateDoc doc = new GenerateDoc("Test");
+	//	doc.toExcel(data);
+	}
+
+	//Instance permettant de rechercher selon une m�thode r�cursive les fichiers d'un dossier.
+
+	//-----------------------------------------------------------------------------------------------------	
+}
