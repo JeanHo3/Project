71eaf4afafede0b738d50f053a48d881cc549546
JeanHo3 <jean.hourmant@gmail.com>
2017-08-28 18:52:11 +0200

    Ajout des commentaires.

---
+++ Arduino/Arduino19082017.ino
@@ -0,0 +1,85 @@
+#include "DHT.h"
+#include <TimeLib.h>
+#include <Wire.h>
+
+#define DHTPIN A0     // what pin we're connected to
+#define moisensPin A1
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE);
+
+int motorPin = 3;
+int watertime = 5;
+int waittime = 5;
+int watered = false;
+String output;
+
+void setup()
+{
+    Serial.begin(9600);
+    dht.begin();
+    Wire.begin(8);                // join i2c bus with address #8
+    //Wire.onRequest(requestEvent); // register event
+    Wire.onReceive(receiveEvent);
+    pinMode(motorPin, OUTPUT);
+}
+
+void loop()
+{
+
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    int moisensValue = analogRead(moisensPin);
+
+    // check if returns are valid, if they are NaN (not a number) then something went wrong!
+    if (isnan(t) || isnan(h))
+    {
+        Serial.println("Failed to read from DHT");
+    }
+    else
+    {
+        output = h;
+        output.concat(";");
+        output.concat(String(t));
+        output.concat(";");
+        output.concat(String(moisensValue));
+        output.concat(";");
+        output.concat(String(watered));
+        if(t>20 && moisensValue>100 && watered == false)
+        {
+          digitalWrite(motorPin, HIGH);
+          delay(watertime*1000);
+          digitalWrite(motorPin, LOW);
+          watered = true;
+        }
+        delay(10000);
+    }
+}
+
+void requestEvent() {
+  char newOut[output.length()+1];
+  output.toCharArray(newOut, output.length()+1);
+  Wire.write(newOut); // respond with message
+  // as expected by master
+}
+
+void receiveEvent(int howMany){
+  int x = Wire.read();
+  if(x==1)
+  {
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    int moisensValue = analogRead(moisensPin);
+    output = h;
+        output.concat(";");
+        output.concat(String(t));
+        output.concat(";");
+        output.concat(String(moisensValue));
+        output.concat(";");
+        output.concat(String(watered));
+        char newOut[output.length()+1];
+  output.toCharArray(newOut, output.length()+1);
+  Wire.write(newOut); // respond with message
+
+  }
+  Serial.println(x);
+}
--- Raspberry/test.js
+++ Raspberry/test.js
@@ -1,12 +1,16 @@
+//Variables de connection au device Arduino
 var i2c = require ('i2c');
 var address = 0x18;
 var device = new i2c(address, {device: '/dev/i2c-1'});
 var devices = [device];
-var getdatas;
+
+//Variables de récupération des données de l'Arduino 1
 var humi;
 var temp = 0.00;
 var mois;
 var askdata = 0;
+
+//Variables de connection à la base de données SQL
 var mysql = require('mysql');
 var con = mysql.createConnection({
 	host : "localhost",
@@ -15,23 +19,29 @@ var con = mysql.createConnection({
 	database : "Arduino1"
 });
 
+//Set du numéro de device par la commande sudo i2cdetect -y 1
 device.setAddress(0x8);
 
+//Connection à la base SQL avec collecte erreur
 con.connect(function(err) {
 	if(err) console.log("Erreur de connection");
 	else	console.log("Connected!");
 });
 
+//Fonction de timeout des requêtes de la fonction handleread (lecture Arduino)
 function handleTimeout(){
 	setTimeout(function() { handleRead(); }, 1000 );
 }
 
+//Fonction de lecture des données Arduino toutes les 2 minutes
 function handleRead(){
 	var date = new Date();
 	var min = date.getMinutes();
 	var sec = date.getSeconds();
 
 	if(min%2==0 && askdata==0){
+
+		//On lit les données du Arduino (humidité,température,moisture)
 		devices[0].readBytes(null,17,function(err,res){
 			if(err) console.log("Erreur de lecture Arduino");
 			else{
@@ -45,6 +55,8 @@ function handleRead(){
 				addInBDD('MO',mois);
 			}
 		});
+
+		//Ecriture d'un bit de donnée 1.
 		devices[0].writeByte(0x1,function(err) {
 			if(err) console.log(err);
 		});
@@ -54,20 +66,23 @@ function handleRead(){
 	if((min%2)!=0){
 		askdata = 0;
 	}
-
+	//Appel de la fonction de gestion timeout qui boucle sur handleRead
 	handleTimeout();
 }
 
+//Fonction de recupération de la valeur des minutes de la date actuelle
 function getDateMin(){
 	min = (min < 10 ? "0" : "") + min;
 	return min;
 }
 
+//Fonction de récupération de la valeur des secondes de la date actuelle
 function getDateSec(){
 	sec = (sec < 10 ? "0" : "") + sec;
 	return sec;
 }
 
+//Fonction d'ajout des datas récupérées dans la base de données mysql
 function addInBDD(id,value) {
   var sql = "INSERT INTO donnees (typecapteur, valeur) VALUES ('" + id + "'," + value +");";
   con.query(sql, function (err, result) {
@@ -75,4 +90,5 @@ function addInBDD(id,value) {
   });
 }
 
+//Appel de la fonction de gestion timeout qui boucle sur handleRead
 handleTimeout();
